20bb8a276a8483b82ef4165e5ea719c97df8fff6
Ted M. Young <tedyoung@gmail.com>
2020-09-03 19:05:17 -0700

    Added "buy" and value feature.

--- src/main/java/com/tedmyoung/portfolio/Portfolio.java
+++ src/main/java/com/tedmyoung/portfolio/Portfolio.java
@@ -1,7 +1,27 @@
 package com.tedmyoung.portfolio;
 
+import java.time.LocalDate;
+
 public class Portfolio {
+  private int value = 0;
+  private String symbol;
+  private int qty;
+  private int price;
+  private LocalDate date;
+
   public int value() {
-    return 0;
+    return value;
+  }
+
+  public void buy(String symbol, int qty, int price, LocalDate date) {
+    this.symbol = symbol;
+    this.qty = qty;
+    this.price = price;
+    this.date = date;
+    value += qty * price;
+  }
+
+  public String display() {
+    return symbol + " " + qty + " $" + price + " " + date;
   }
 }
--- src/test/java/com/tedmyoung/portfolio/PortfolioTest.java
+++ src/test/java/com/tedmyoung/portfolio/PortfolioTest.java
@@ -2,7 +2,9 @@ package com.tedmyoung.portfolio;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class PortfolioTest {
   @Test
@@ -12,4 +14,36 @@ public class PortfolioTest {
     assertThat(portfolio.value())
         .isZero();
   }
+
+  @Test
+  public void buy100SharesOfStockAt125ValuedAt12_500() throws Exception {
+    Portfolio portfolio = new Portfolio();
+
+    portfolio.buy("AAPL", 100, 125, LocalDate.of(2020, 9, 3));
+
+    assertThat(portfolio.value())
+        .isEqualTo(125 * 100);
+  }
+
+  @Test
+  public void buySharesTwicePortfolioValuedAtSumOfShares() throws Exception {
+    Portfolio portfolio = new Portfolio();
+
+    portfolio.buy("AAPL", 100, 125, LocalDate.of(2020, 9, 3));
+    portfolio.buy("AMD", 200, 90, LocalDate.of(2020, 9, 3));
+
+    assertThat(portfolio.value())
+        .isEqualTo(100 * 125 + 200 * 90);
+  }
+
+  @Test
+  public void givenSingleStockHoldingShowsHoldingInformation() throws Exception {
+    Portfolio portfolio = new Portfolio();
+    portfolio.buy("AAPL", 100, 125, LocalDate.of(2020, 9, 3));
+
+    assertThat(portfolio.display())
+        .isEqualTo("AAPL 100 $125 2020-09-03");
+  }
+
+
 }
